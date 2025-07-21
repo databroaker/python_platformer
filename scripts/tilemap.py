4c9786b531d53210214a13e432c232c7cbeb3d66
Myk3le <myk3le@proton.me>
2025-07-21 08:47:56 -0700

    Basic level editor finished. Adding autotiling now.

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,8 +1,14 @@
 import pygame
+import json
+
+AUTOTILE_MAP = {
+
+}
 
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1,), (0, 1), (1, 1)]
 
 PHYSICS_TILES = {"grass", "stone"}
+AUTOTILE_TYPES = {"grass", "stone"}
 
 class TileMap:
     def __init__(self, game, tile_size=16):
@@ -10,10 +16,6 @@ class TileMap:
         self.tile_size = tile_size
         self.tilemap = {}
         self.offgrid_tiles = []
-
-        for i in range(10):
-            self.tilemap[str(3+i) + ";10"] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
-            self.tilemap["10;" + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
     def tiles_around(self, pos):
         tiles = []
@@ -24,12 +26,36 @@ class TileMap:
                 tiles.append(self.tilemap[check_loc])
         return tiles
 
+    def save(self, path):
+        f = open(path, "w")
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close()
+
+    def load(self, path):
+        f = open(path, "r")
+        map_data = json.load(f)
+        f.close()
+
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+
     def physics_rects_around(self, pos):
         rects = []
         for tile in self.tiles_around(pos):
             if tile["type"] in PHYSICS_TILES:
                 rects.append(pygame.Rect(tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
+
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                check_loc = str(tile['pos'][0] + shift[0] + ";" + tile['pos'][1] + shift[1])
+                if check_loc in self.tilemap:
+
+
 
     def render(self, surf, offset=(0, 0)):
         for tile in self.offgrid_tiles:
