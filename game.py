461054c72d3a2521955d7645c2099db519e74753
Myk3le <myk3le@proton.me>
2025-07-22 08:52:51 -0700

    Level editor done. Autotile done. Added leaf particles.

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
+import math
+import random
 import sys
 import pygame
 from scripts.entities import PhysicsEntity, Player
 from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import TileMap
 from scripts.clouds import Clouds
+from scripts.particle import Particle
 
 class Game:
     def __init__(self):
@@ -27,12 +30,20 @@ class Game:
             "player/run": Animation(load_images('entities/player/run'), img_dur=4),
             "player/jump": Animation(load_images('entities/player/idle')),
             "player/slide": Animation(load_images('entities/player/slide')),
-            "player/wall_slide": Animation(load_images('entities/player/wall_slide'))
+            "player/wall_slide": Animation(load_images('entities/player/wall_slide')),
+            "particle/leaf": Animation(load_images('particles/leaf'), img_dur=20, loop=False)
         }
 
         self.clouds = Clouds(self.assets["clouds"], count=16)
         self.player = Player(self, (50, 50), (8, 15))
         self.tilemap = TileMap(self, tile_size=16)
+        self.tilemap.load('map.json')
+
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+        self.particles = []
+
         self.scroll = [0, 0]
 
     def run(self):
@@ -43,6 +54,11 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
+            for rect in self.leaf_spawners:
+                if random.random() * 49999 < rect.width * rect.height:
+                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+
             self.clouds.update()
             self.clouds.render(self.display, offset=render_scroll)
 
@@ -51,6 +67,14 @@ class Game:
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
 
             self.player.render(self.display, offset=render_scroll)
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.type == 'leaf':
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -2,7 +2,15 @@ import pygame
 import json
 
 AUTOTILE_MAP = {
-
+    tuple(sorted([(1, 0), (0, 1)])): 0,
+    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
+    tuple(sorted([(-1, 0), (0, 1)])): 2,
+    tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
+    tuple(sorted([(-1, 0), (0, -1)])): 4,
+    tuple(sorted([(-1, 0), (0, -1), (1, 0)])): 5,
+    tuple(sorted([(1, 0), (0, -1)])): 6,
+    tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
+    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 8,
 }
 
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1,), (0, 1), (1, 1)]
@@ -17,10 +25,30 @@ class TileMap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
+    def extract(self, id_pairs, keep=False):
+        matches = []
+        for tile in self.offgrid_tiles.copy():
+            if (tile['type'], tile['variant']) in id_pairs:
+                matches.append(tile.copy())
+                if not keep:
+                    self.offgrid_tiles.remove(tile)
+
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            if (tile['type'], tile['variant']) in id_pairs:
+                matches.append(tile.copy())
+                matches[-1]['pos'] = matches[-1]['pos'].copy()
+                matches[-1]['pos'][0] *= self.tile_size
+                matches[-1]['pos'][1] *= self.tile_size
+                if not keep:
+                    del self.tilemap[loc]
+        return matches
+
     def tiles_around(self, pos):
         tiles = []
         tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
         for offset in NEIGHBOR_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
             check_loc = str(tile_loc[0] + offset[0]) + ";" + str(tile_loc[1] + offset[1])
             if check_loc in self.tilemap:
                 tiles.append(self.tilemap[check_loc])
@@ -52,9 +80,13 @@ class TileMap:
             tile = self.tilemap[loc]
             neighbors = set()
             for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
-                check_loc = str(tile['pos'][0] + shift[0] + ";" + tile['pos'][1] + shift[1])
+                check_loc = str(tile['pos'][0] + shift[0]) + ";" + str(tile['pos'][1] + shift[1])
                 if check_loc in self.tilemap:
-
+                    if self.tilemap[check_loc]['type'] == tile['type']:
+                        neighbors.add(shift)
+            neighbors = tuple(sorted(neighbors))
+            if (tile['type'] in AUTOTILE_TYPES) and (neighbors in AUTOTILE_MAP):
+                tile['variant'] = AUTOTILE_MAP[neighbors]
 
 
     def render(self, surf, offset=(0, 0)):
